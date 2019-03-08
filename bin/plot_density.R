bb2a879054f15017cde37edc18f192a02cb2d4c3
Universe <universe@MacBook-Air.local>
2019-03-08 14:08:07 -0500

    NB model decide count threshold

--- bin/plot_density.R
+++ bin/plot_density.R
@@ -3,16 +3,58 @@ args = commandArgs(trailingOnly=TRUE)
 index_matrix = args[1]
 count_threshold = as.numeric(args[2])
 
-
 index = read.table(index_matrix, header = F)
 
 index = apply(index, 1, function(x) paste(x[5:dim(index)[2]], collapse='_'))
-#print(head(index))
 index_count = table(index)
-#print(head(index_count))
+print(summary(as.matrix(index_count)))
+print(length(index_count))
 
+
+if (length(index_count)>100){
+### select data without top X quantile
+top = 0.99
+mean_99 = mean(index_count[index_count<quantile(index_count, top)])
+var_99 = var(index_count[index_count<quantile(index_count, top)])
+sd_99 = sd(index_count[index_count<quantile(index_count, top)])
+print(mean_99)
+print(var_99)
+print(summary(as.matrix(index_count[index_count<quantile(index_count, top)])))
+
+### get NB size and prob
+size = (mean_99^2)/(var_99 - mean_99)
+prob=mean_99/var_99
+
+### get NB p-val
+pvec = pnbinom(index_count, size=size, prob=prob, lower.tail = FALSE)
+print(summary(as.matrix(pvec)))
+### FDR pval
+padjvec = p.adjust(pvec, method='fdr')
+print(summary(as.matrix(padjvec)))
+### get NB count thresh
+counts_pfdr = cbind(index_count, padjvec)
+if (sum(padjvec<1e-2)>0){
+	NB_count_thresh = min(counts_pfdr[padjvec<1e-2,1])
+} else if (sum(padjvec<0.05)>0) {
+	NB_count_thresh = min(counts_pfdr[padjvec<0.05,1])
+} else if (sum(pvec<0.01)>0) {
+	NB_count_thresh = min(counts_pfdr[pvec<0.01,1])
+} else if (sum(pvec<0.05)>0) {
+	NB_count_thresh = min(counts_pfdr[pvec<0.05,1])
+} else {
+	NB_count_thresh = count_threshold
+	print('NB model fail, use user provide count_threshold')
+}
+} else{
+	NB_count_thresh = 3
+}
+
+###### plot density
 png('density_index_count.png', width = 8, height = 8, units = 'in', res = 300)
 plot(density(log2(index_count)), main='density plot of index counts (log2 scale)')
 abline(v=log2(count_threshold), col='red', lwd=1.5, lty=2)
+abline(v=log2(NB_count_thresh), col='blue', lwd=1.5, lty=2)
 dev.off()
+
+write.table(as.matrix(NB_count_thresh), paste(index_matrix, '.NB_count_thresh.txt', sep=''), quote=F, col.names=F, row.names=F, sep='\t')
 
--- runall_commandline.sh
+++ runall_commandline.sh
@@ -7,5 +7,5 @@ output_folder='/Users/universe/Documents/2018_BG/snapshot/test_data/output_resul
 ### run snapshot (CORE!!!)
 echo 'run snapshot :o'
 cd $input_folder
-time python $script_folder'snapshot_v.0.3.py' -p peak_list.txt -n snapshot -t 5 -s signal_list.txt -l F -z F -x 0.01 -f function_list.txt -m mostfreq -c function_color_list.txt -e cd_tree.txt -i $input_folder -o $output_folder -b $script_folder -q 1
+time python $script_folder'snapshot_v.0.4.py' -p peak_list.txt -n snapshot -t 1 -s signal_list.txt -l F -z F -x 0.01 -f function_list.txt -m mostfreq -c function_color_list.txt -e cd_tree.txt -i $input_folder -o $output_folder -b $script_folder -q 1
 echo 'complete :)'
